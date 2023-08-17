4507cfe55e5fe87ad13ff7b5e2916ba37c8b2c3f
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-08-16 21:01:36 -0600

    feat(default.nix): building from local path

--- default.nix
+++ default.nix
@@ -3,21 +3,14 @@
   buildGoModule,
   lib,
 }: let
-  owner = "luisnquin";
-  version = "0.4.4";
+  version = "v0.4.5";
   commit = "0ab42428cd5e689160a7592496d11288bf756441";
 in
   buildGoModule rec {
     pname = "senv";
     inherit version;
 
-    src = fetchFromGitHub {
-      inherit owner;
-
-      repo = pname;
-      rev = "v${version}";
-      sha256 = "100q65248sc32qrmx5385p0cw7ixyxaw3a6wa2rnq4kaahwmhsyf";
-    };
+    src = ./.;
 
     vendorSha256 = "sha256-C33Kj6PXoXa3OuH1ZP5kDJGR+BNaqbDrDGNtVpYgHZU=";
     doCheck = true;
@@ -27,7 +20,7 @@ in
 
     meta = with lib; {
       description = "Switch your .env file from the command line";
-      homepage = "https://github.com/${owner}/${pname}";
+      homepage = "https://github.com/luisnquin/${pname}";
       license = licenses.mit;
       maintainers = with maintainers; [luisnquin];
     };
