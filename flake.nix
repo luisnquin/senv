986589f1ffa493c915477c44a9117fba02da9ffc
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-08-17 20:13:38 -0600

    feat(flake.nix): use of flake-utils.lib.eachDefaultSystem

--- flake.nix
+++ flake.nix
@@ -3,25 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
-    pkgs = import nixpkgs {
-      inherit system;
-    };
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-    system = "x86_64-linux";
-    app = pkgs.callPackage ./default.nix {};
-  in {
-    devShell = pkgs.mkShell {
-      buildInputs = [app];
-    };
+        defaultPackage = pkgs.callPackage ./default.nix {};
+      in {
+        inherit defaultPackage;
 
-    defaultPackage.${system} = app;
+        defaultApp = flake-utils.lib.mkApp {
+          drv = defaultPackage;
+        };
 
-    packages.${system}."senv" = app;
-  };
+        devShell = pkgs.mkShell {
+          buildInputs = [defaultPackage];
+        };
+      }
+    );
 }
