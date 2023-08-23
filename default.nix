18740306aa851ab8cdbdb6ea869b743a48248f8f
luisnquin <luisnquin@users.noreply.github.com>
2023-08-23 02:09:32 +0000

    chore(nix): updated version and commit

--- default.nix
+++ default.nix
@@ -3,8 +3,8 @@
   buildGoModule,
   lib,
 }: let
-  version = "v0.5.1";
-  commit = "d1eaa3e287f5b619424cc6cdaf1e62791d49a277";
+  version = "v0.5.2";
+  commit = "28e7f0baf4dd7ff1232ba98143e157d2c435faf5";
 in
   buildGoModule rec {
     pname = "senv";
